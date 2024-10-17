716ab6df25d864b5d7a2d9dd3d48e2989b22caba
alex2000d <demarcoalessandro777@gmail.com>
2024-10-17 22:12:01 +0200

    [UPD] create the model migraton and mailable

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Lead;
+
+class LeadController extends Controller
+{
+    //
+}
---
+++ resources/views/emails/contact-email.blade.php
@@ -0,0 +1,9 @@
+<h1>Ciao admin</h1>
+<p>Hai ricevuto una nuova email da <br>
+    nome: {{ $lead->name }} <br>
+    cognome: {{ $lead->surname }} <br>
+    telefono: {{$lead->phone}} <br>
+    email: {{ $lead->email}} <br>
+    contenuto: {{$lead->content}}
+
+</p>
